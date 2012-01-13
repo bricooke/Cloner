78fccc6c2e2f36e6f2df1e5dd78bd3594c53739c
Brian Cooke <brian@madebyrocket.com>
2012-01-13 04:30:38 -0800

    Initial Commit

---
+++ Clone/RSCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSCAppDelegate.h
+//  Clone
+//
+//  Created by Brian Cooke on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RSCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Clone/RSCAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  RSCAppDelegate.m
+//  Clone
+//
+//  Created by Brian Cooke on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RSCAppDelegate.h"
+
+@implementation RSCAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
