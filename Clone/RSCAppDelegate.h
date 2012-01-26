fb35782d9b0470108e8e88a8b74a800b2574cd0e
Brian Cooke <brian@madebyrocket.com>
2012-01-26 10:29:41 -0800

    Remove all the junk where the user can paste in a URL. I just want the bookmarklet or drag & drop to dock icon.

--- Clone/RSCAppDelegate.h
+++ Clone/RSCAppDelegate.h
@@ -4,15 +4,6 @@
 
 @interface RSCAppDelegate : NSObject <NSApplicationDelegate>
 
-@property (assign) IBOutlet NSWindow *window;
-@property (weak) IBOutlet NSTextField *cloneURLTextField;
-@property (weak) IBOutlet NSProgressIndicator *progressBar;
-@property (weak) IBOutlet NSButton *cloneButton;
-@property (weak) IBOutlet NSTextField *destinationLabel;
-@property (nonatomic, assign) BOOL cloneOnActivate;
-
-- (IBAction)clone:(id)sender;
-- (IBAction)cloneViaKeyboard:(id)sender;
-- (IBAction)browse:(id)sender;
+@property (weak) IBOutlet NSProgressIndicator *progressIndicator;
 
 @end
--- Clone/RSCAppDelegate.m
+++ Clone/RSCAppDelegate.m
@@ -6,13 +6,7 @@
 
 
 @implementation RSCAppDelegate
-
-@synthesize window = _window;
-@synthesize cloneURLTextField = _cloneURLTextField;
-@synthesize progressBar = _progressBar;
-@synthesize cloneButton = _cloneButton;
-@synthesize destinationLabel = _destinationLabel;
-@synthesize cloneOnActivate = _cloneOnActivate;
+@synthesize progressIndicator;
 
 - (void)applicationWillFinishLaunching:(NSNotification *)notification
 {
@@ -31,70 +25,30 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {    
-    if (RSC_SETTINGS.destinationIsDownloads) {
-        self.destinationLabel.stringValue = @"Downloads";
-    } else {
-        self.destinationLabel.stringValue = RSC_SETTINGS.destinationPath;
-    }
 }
 
 - (void)applicationDidBecomeActive:(NSNotification *)notification
 {
-    if (self.cloneOnActivate) {
-        // don't interrupt it!
-        self.cloneOnActivate = NO;
-        return;
-    }
-    
-    NSArray *classes        = [[NSArray alloc] initWithObjects:[NSString class], nil];
-    NSDictionary *options   = [NSDictionary dictionary];
-    NSArray *copiedItems    = [[NSPasteboard generalPasteboard] readObjectsForClasses:classes options:options];
-
-    if ([copiedItems count] == 0) {
-        [self.cloneURLTextField becomeFirstResponder];
-        return; // ignore contents.
-    }
-    
-    NSString *clipboard = [copiedItems objectAtIndex:0];
-    
-    NSError *error = NULL;
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\S*$"
-                                                                           options:NSRegularExpressionCaseInsensitive
-                                                                             error:&error];
-    if ([regex numberOfMatchesInString:clipboard
-                               options:0
-                                 range:NSMakeRange(0, [clipboard length])] > 0) {
-        // a match.
-        self.cloneURLTextField.stringValue = clipboard;
-        [self.cloneURLTextField becomeFirstResponder];
-        [[self.cloneURLTextField currentEditor] setSelectedRange:NSMakeRange(0, [self.cloneURLTextField.stringValue length])];
-    } else {
-        [self.cloneURLTextField becomeFirstResponder];
-        [[self.cloneURLTextField currentEditor] setSelectedRange:NSMakeRange(0, [self.cloneURLTextField.stringValue length])];
-    }
 }
 
 
-#pragma mark - actions
-- (IBAction)clone:(id)sender 
+- (void)cloneUrl:(NSString *)repoURL
 {
-    [self.progressBar setIndeterminate:YES];
-    [self.progressBar startAnimation:self];
-
-    NSString *repoURL = self.cloneURLTextField.stringValue;
+    DLog(@"Cloning: %@", repoURL);
+    
     RSCGitCloner *cloner = [[RSCGitCloner alloc] initWithRepositoryURL:repoURL];
    
     void (^cloneProgressBlock)(NSInteger progress) = ^(NSInteger progress) {
         DLog(@"Progress!: %lu", progress);
         
-        [self.progressBar setIndeterminate:NO];
-        [self.progressBar setDoubleValue:progress];
+        [self.progressIndicator setIndeterminate:NO];
+        [self.progressIndicator setDoubleValue:progress];
     };
     
     void (^cloneCompletionBlock)(NSInteger responseCode) = ^(NSInteger responseCode) {
         DLog(@"Completion block...");
-        [self.progressBar setDoubleValue:0.0];
-        [self.progressBar stopAnimation:self];
+        [self.progressIndicator setDoubleValue:0.0];
+        [self.progressIndicator stopAnimation:self];
         
         if (responseCode == kRSCGitClonerErrorNone) {
             [[NSWorkspace sharedWorkspace] openFile:cloner.destinationPath withApplication:@"Finder"];
@@ -116,35 +70,13 @@
     [cloner cloneWithProgressBlock:cloneProgressBlock andCompletionBlock:cloneCompletionBlock];
 }
 
-- (IBAction)cloneViaKeyboard:(id)sender 
-{
-    [self clone:self];
-}
-
-- (IBAction)browse:(id)sender 
-{
-    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
-    openPanel.canChooseFiles = NO;
-    openPanel.canChooseDirectories = YES;
-    openPanel.canCreateDirectories = YES;
-    
-    if ([openPanel runModal] == NSFileHandlingPanelOKButton) {
-        RSC_SETTINGS.destinationPath = [[[openPanel URLs] objectAtIndex:0] path];
-        RSC_SETTINGS.destinationIsDownloads = NO;
-        self.destinationLabel.stringValue = RSC_SETTINGS.destinationPath;
-    }
-}
-
 
 #pragma mark - bookmarklet
 - (void)cloneUrl:(NSAppleEventDescriptor *)event withReplyEvent:(NSAppleEventDescriptor *)replyEvent
 {
-    self.cloneOnActivate = YES;
-    
     NSString *urlAsString = [[event paramDescriptorForKeyword:keyDirectObject] stringValue];
     NSURL *url = [NSURL URLWithString:urlAsString];
-    self.cloneURLTextField.stringValue = [url host];
-    [self clone:self];
+    [self cloneUrl:[url host]];
 }
 
 
@@ -152,11 +84,8 @@
 #pragma mark - drop on dock icon handling
 -(void)cloneFromPasteboard:(NSPasteboard *)pboard userData:(NSString *)userData error:(NSString **)error 
 {
-    self.cloneOnActivate = YES;
-    
     NSString *urlToClone = [pboard stringForType:NSStringPboardType];
-    self.cloneURLTextField.stringValue = urlToClone;
-    [self clone:self];
+    [self cloneUrl:urlToClone];
 }
 
 
